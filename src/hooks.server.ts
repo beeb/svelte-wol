019bc91fa58fecea1a538bd8b15f31a0f6e70bd3
Valentin Bersier <vbersier@gmail.com>
2023-01-11 10:38:51 +0100

    feat: adjust timings

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -2,12 +2,13 @@ import type { Handle } from '@sveltejs/kit'
 import rateLimit from './lib/rateLimit'
 
 const limiter = rateLimit({
-	interval: 10 * 1000, // usage period is 10 seconds
-	uniqueTokenPerInterval: 10 // max 10 users concurrently
+	interval: 60 * 1000, // usage period is 1 minute
+	uniqueTokenPerInterval: 500 // max 500 distinct IPs (to avoid using too much memory)
 })
 
 export const handle: Handle = async ({ event, resolve }) => {
-	let { isRateLimited, limit, remaining } = limiter.check(10, event.getClientAddress())
+	// limit to 100 requests per minute
+	let { isRateLimited, limit, remaining } = limiter.check(100, event.getClientAddress())
 	let response = new Response('rate limited')
 	if (!isRateLimited) {
 		response = await resolve(event)
