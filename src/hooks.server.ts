f066dee6ab8c1afb060016bef9bba164ca9be7d1
Valentin Bersier <vbersier@gmail.com>
2023-01-11 10:36:27 +0100

    fix: use client address

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -2,18 +2,18 @@ import type { Handle } from '@sveltejs/kit'
 import rateLimit from './lib/rateLimit'
 
 const limiter = rateLimit({
-  interval: 10 * 1000, // usage period is 10 seconds
-  uniqueTokenPerInterval: 10, // max 10 users concurrently
+	interval: 10 * 1000, // usage period is 10 seconds
+	uniqueTokenPerInterval: 10 // max 10 users concurrently
 })
 
 export const handle: Handle = async ({ event, resolve }) => {
-  let { isRateLimited, limit, remaining } = limiter.check(10, 'asdf')
-  let response = new Response('rate limited')
-  if (!isRateLimited) {
-    response = await resolve(event);
-  }
-  response.headers.set('X-RateLimit-Limit', `${limit}`)
-  response.headers.set('X-RateLimit-Remaining', `${remaining}`)
-  
-  return response;
+	let { isRateLimited, limit, remaining } = limiter.check(10, event.getClientAddress())
+	let response = new Response('rate limited')
+	if (!isRateLimited) {
+		response = await resolve(event)
+	}
+	response.headers.set('X-RateLimit-Limit', `${limit}`)
+	response.headers.set('X-RateLimit-Remaining', `${remaining}`)
+
+	return response
 }
--- src/lib/rateLimit.ts
+++ src/lib/rateLimit.ts
@@ -1,32 +1,31 @@
 import LRU from 'lru-cache'
 
 type Options = {
-  uniqueTokenPerInterval?: number
-  interval?: number
+	uniqueTokenPerInterval?: number
+	interval?: number
 }
 
 export default function rateLimit(options?: Options) {
-  const tokenCache = new LRU({
-    max: options?.uniqueTokenPerInterval || 500,
-    ttl: options?.interval || 60000,
-  })
+	const tokenCache = new LRU({
+		max: options?.uniqueTokenPerInterval || 500,
+		ttl: options?.interval || 60000
+	})
 
-  return {
-    check: (limit: number, token: string) =>
-      {
-        const tokenCount = (tokenCache.get(token) as number[]) || [0]
-        if (tokenCount[0] === 0) {
-          tokenCache.set(token, tokenCount)
-        }
-        tokenCount[0] += 1
+	return {
+		check: (limit: number, token: string) => {
+			const tokenCount = (tokenCache.get(token) as number[]) || [0]
+			if (tokenCount[0] === 0) {
+				tokenCache.set(token, tokenCount)
+			}
+			tokenCount[0] += 1
 
-        const currentUsage = tokenCount[0]
-        const isRateLimited = currentUsage >= limit
-        return {
-          isRateLimited,
-          limit: limit,
-          remaining: isRateLimited ? 0 : limit - currentUsage
-        }
-      },
-  }
+			const currentUsage = tokenCount[0]
+			const isRateLimited = currentUsage >= limit
+			return {
+				isRateLimited,
+				limit: limit,
+				remaining: isRateLimited ? 0 : limit - currentUsage
+			}
+		}
+	}
 }
