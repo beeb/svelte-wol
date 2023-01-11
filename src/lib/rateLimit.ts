f4bbed318e346c6a6612a0d71974bc2d25ac09e5
Valentin Bersier <vbersier@gmail.com>
2023-01-11 10:32:41 +0100

    feat: rate limiting

---
+++ src/lib/rateLimit.ts
@@ -0,0 +1,32 @@
+import LRU from 'lru-cache'
+
+type Options = {
+  uniqueTokenPerInterval?: number
+  interval?: number
+}
+
+export default function rateLimit(options?: Options) {
+  const tokenCache = new LRU({
+    max: options?.uniqueTokenPerInterval || 500,
+    ttl: options?.interval || 60000,
+  })
+
+  return {
+    check: (limit: number, token: string) =>
+      {
+        const tokenCount = (tokenCache.get(token) as number[]) || [0]
+        if (tokenCount[0] === 0) {
+          tokenCache.set(token, tokenCount)
+        }
+        tokenCount[0] += 1
+
+        const currentUsage = tokenCount[0]
+        const isRateLimited = currentUsage >= limit
+        return {
+          isRateLimited,
+          limit: limit,
+          remaining: isRateLimited ? 0 : limit - currentUsage
+        }
+      },
+  }
+}
